0d06db7078df4d467b07eea4a4448fa7956f5431
Simon Ma <simon.ma@valtech.com>
2022-11-09 16:40:30 +0800

    test ut

---
+++ src/test/java/com/flying/FlyingControllerTest.java
@@ -0,0 +1,14 @@
+package com.flying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyingControllerTest {
+
+    @Test
+    void testInit(){
+        int i =1/0;
+    }
+
+}
