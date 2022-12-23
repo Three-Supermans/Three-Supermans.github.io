83bd1a0d8acfe978c1e3b50651eca11927a49ee7
Mars Zhu <mars.zhu@valtech.com>
2022-12-23 17:17:08 +0800

    add invoke ms api

--- js/main.js
+++ js/main.js
@@ -4,44 +4,30 @@ const App = {
             cacheTime: 3600000,
             isLogged: false,
             todoInput: "",
-            toDoListTableData: [
-                {
-                    finished: false,
-                    content: 'ODP Live SOP late shift',
-                },
-                {
-                    finished: false,
-                    content: 'Fix the vulnerability of Spring framework in SRVADM',
-                },
-                {
-                    finished: false,
-                    content: '[SRVADM] New database table to store timestamp,SopId and userId',
-                },
-                {
-                    finished: false,
-                    content: 'integrate renovate bot for MOCKS',
-                },
-                {
-                    finished: true,
-                    content: 'integrate renovate bot for ECHO',
-                },
-            ],
+            context: "https://graph.microsoft.com/v1.0",
+            getTodoListPath: "/me/todo/lists/",
+            getTasksPath: "/tasks/",
+            todoList: [],
+            toDoListTableData: [],
+            currentListId:"",
         }
     },
     mounted() {
+
         if (window.location.href.includes("access_token")) {
-            let token = window.location.href.split("=")[1].split("&")[0]
-            console.log('外部进入：' + token)
+            let token = decodeURIComponent(window.location.href).split("=")[1].split("&")[0]
             localStorage.setItem("lastTokenTime", new Date().valueOf().toString())
             localStorage.setItem("accessToken", token)
             this.isLogged = true
+
+            this.refreshTodoList()
         }
     },
     methods: {
 
         handleSubmit() {
             if (localStorage.getItem("accessToken") == null || new Date().valueOf() - parseInt(localStorage.getItem("lastTokenTime")) > this.cacheTime) {
-                window.open('https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=44f6e5be-cecd-4c50-a615-3a2fb4b18b3e&scope=files.readwrite.all&response_type=token' +
+                window.open('https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=44f6e5be-cecd-4c50-a615-3a2fb4b18b3e&scope=Tasks.ReadWrite&response_type=token' +
                     '&redirect_uri=http://localhost:3000', '_self')
             }
         },
@@ -49,26 +35,90 @@ const App = {
             localStorage.clear()
             this.isLogged = false
         },
-        statusChange(index, preStatus) {
-            this.toDoListTableData[index].finished = !preStatus
-            this.toDoListTableData.sort(this.compareIfFinished)
+        statusChange(index) {
+            //调api改status
+            let that = this
+            let taskId = this.toDoListTableData[index].id
+            let todoListId = this.currentListId
+            let taskStatus
+            if (this.toDoListTableData[index].status == "completed") {
+                taskStatus = "notStarted"
+            } else {
+                taskStatus = "completed"
+            }
+            axios.patch(this.context + this.getTodoListPath + todoListId + this.getTasksPath + taskId,
+                {
+                    status: taskStatus
+                },
+                {
+                    headers: {
+                        Authorization: "Bearer " + localStorage.getItem("accessToken"),
+                        "Content-Type": "application/json"
+                    },
+
+                }).then(res => {
+                that.getTasksById(todoListId)
+                that.toDoListTableData.sort(this.compareIfFinished)
+            }).catch(err => {
+                console.log('错误' + err)
+            })
         },
         compareIfFinished(a, b) {
-            if (a.finished && !b.finished) {
+            if (a.status == "completed" && b.status !== "completed") {
                 return 1
-            } else if (b.finished && !a.finished) {
+            } else if (b.status == "completed" && a.status !== "completed") {
                 return -1
             } else {
                 return 0;
             }
+
         },
         addTodo() {
-            this.toDoListTableData.unshift({
-                finished: false,
-                content: this.todoInput,
+            let that = this
+            axios.post(this.context + this.getTodoListPath + this.currentListId + this.getTasksPath, {
+                status: "notStarted",
+                title: that.todoInput
+            }, {
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("accessToken")
+                }
+            }).then(res => {
+                this.getTasksById(this.currentListId)
+                this.todoInput = ""
+            }).catch(err => {
+                console.log('错误' + err)
             })
-            this.todoInput = ""
         },
+        refreshTodoList() {
+            let that = this
+            that.menuList = []
+            axios.get(this.context + this.getTodoListPath, {
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("accessToken")
+                }
+            }).then(res => {
+                that.todoList = res.data.value
+                if (Array.isArray(res.data.value) && res.data.value.length) {
+                    that.getTasksById(res.data.value[0].id)
+                }
+            }).catch(err => {
+                console.log('错误' + err)
+            })
+        },
+        getTasksById(listId) {
+            let that = this
+            that.currentListId = listId
+            axios.get(this.context + this.getTodoListPath + listId + this.getTasksPath, {
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("accessToken")
+                }
+            }).then(res => {
+                that.toDoListTableData = res.data.value
+                this.toDoListTableData.sort(this.compareIfFinished)
+            }).catch(err => {
+                console.log('错误' + err)
+            })
+        }
     },
 
 }
