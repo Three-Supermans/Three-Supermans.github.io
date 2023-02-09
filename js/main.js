3fe6a315d87b2531437cd47bce8848df8ee55e88
Mars Zhu <mars.zhu@valtech.com>
2023-02-09 15:19:01 +0800

    make usable for chrome extensions

--- js/main.js
+++ js/main.js
@@ -9,46 +9,70 @@ const App = {
             getTasksPath: "/tasks/",
             todoList: [],
             toDoListTableData: [],
-            currentListId:"",
+            currentListId: "",
+            tokenInput: "",
         }
     },
     mounted() {
-
-        if (window.location.href.includes("access_token")) {
-            let token = decodeURIComponent(window.location.href).split("=")[1].split("&")[0]
-            localStorage.setItem("lastTokenTime", new Date().valueOf().toString())
-            localStorage.setItem("accessToken", token)
-            this.isLogged = true
-
-            this.refreshTodoList()
+        this.isLogged = localStorage.getItem("isLogged")
+        if (this.isLogged) {
+            if (localStorage.getItem("accessToken") != null &&
+                new Date().valueOf() - parseInt(localStorage.getItem("lastTokenTime")) < this.cacheTime) {
+                this.refreshTodoList()
+            } else {
+                this.isLogged = false
+                localStorage.setItem("isLogged", this.isLogged)
+            }
+        }else{
+            if (window.location.href.includes("access_token")) {
+                let token = decodeURIComponent(window.location.href).split("=")[1].split("&")[0]
+                localStorage.setItem("lastTokenTime", new Date().valueOf().toString())
+                localStorage.setItem("accessToken", token)
+                this.isLogged = true
+                localStorage.setItem("isLogged", this.isLogged)
+                this.refreshTodoList()
+            }
         }
-    },
+    }
+    ,
     methods: {
 
         handleSubmit() {
-            if (localStorage.getItem("accessToken") == null || new Date().valueOf() - parseInt(localStorage.getItem("lastTokenTime")) > this.cacheTime) {
+            if (this.tokenInput !== "") {
+                localStorage.setItem("lastTokenTime", new Date().valueOf().toString())
+                localStorage.setItem("accessToken", this.tokenInput)
+                this.isLogged = true
+                localStorage.setItem("isLogged", this.isLogged)
+                this.refreshTodoList()
+            } else if (localStorage.getItem("accessToken") == null || new Date().valueOf() - parseInt(localStorage.getItem("lastTokenTime")) >= this.cacheTime) {
+                //get token
                 window.open('https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=44f6e5be-cecd-4c50-a615-3a2fb4b18b3e&scope=Tasks.ReadWrite&response_type=token' +
-                    '&redirect_uri=http://localhost:3000', '_self')
+                    '&redirect_uri=http://localhost:3000', '_blank')
             }
-        },
+        }
+        ,
         logout() {
-            localStorage.clear()
             this.isLogged = false
-        },
+            localStorage.clear()
+        }
+        ,
         statusChange(index) {
             //调api改status
+            console.log(index)
+            console.log(this.toDoListTableData[index])
             let that = this
             let taskId = this.toDoListTableData[index].id
             let todoListId = this.currentListId
-            let taskStatus
+            console.log(this.toDoListTableData[index].status)
             if (this.toDoListTableData[index].status == "completed") {
-                taskStatus = "notStarted"
+                this.toDoListTableData[index].status = "notStarted"
             } else {
-                taskStatus = "completed"
+                this.toDoListTableData[index].status = "completed"
             }
+            that.toDoListTableData.sort(this.compareIfFinished)
             axios.patch(this.context + this.getTodoListPath + todoListId + this.getTasksPath + taskId,
                 {
-                    status: taskStatus
+                    status: this.toDoListTableData[index].status
                 },
                 {
                     headers: {
@@ -58,11 +82,11 @@ const App = {
 
                 }).then(res => {
                 that.getTasksById(todoListId)
-                that.toDoListTableData.sort(this.compareIfFinished)
             }).catch(err => {
                 console.log('错误' + err)
             })
-        },
+        }
+        ,
         compareIfFinished(a, b) {
             if (a.status == "completed" && b.status !== "completed") {
                 return 1
@@ -72,9 +96,14 @@ const App = {
                 return 0;
             }
 
-        },
+        }
+        ,
         addTodo() {
             let that = this
+            this.toDoListTableData.unshift({
+                status: "notStarted",
+                title: this.todoInput,
+            })
             axios.post(this.context + this.getTodoListPath + this.currentListId + this.getTasksPath, {
                 status: "notStarted",
                 title: that.todoInput
@@ -88,10 +117,10 @@ const App = {
             }).catch(err => {
                 console.log('错误' + err)
             })
-        },
+        }
+        ,
         refreshTodoList() {
             let that = this
-            that.menuList = []
             axios.get(this.context + this.getTodoListPath, {
                 headers: {
                     Authorization: "Bearer " + localStorage.getItem("accessToken")
@@ -104,7 +133,8 @@ const App = {
             }).catch(err => {
                 console.log('错误' + err)
             })
-        },
+        }
+        ,
         getTasksById(listId) {
             let that = this
             that.currentListId = listId
@@ -119,7 +149,8 @@ const App = {
                 console.log('错误' + err)
             })
         }
-    },
+    }
+    ,
 
 }
 const app = Vue.createApp(App);
